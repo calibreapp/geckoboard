f6b9af3d6db40f91e77e1392c02258015266d5e1
Ben Schwarz <ben@germanforblack.com>
2017-06-13 14:31:48 +1000

    Added bodyparser and start script

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require('dotenv').config()
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const geckoboard = require('geckoboard')(process.env.GECKOBOARD_API_KEY)
 const metricWhitelist = process.env.METRIC_WHITELIST.split(',')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.bodyParser())
+app.use(bodyParser.json())
 
 app.post('/webhook', (req, res) => {
   metrics = new Metrics({ payload: req.body, metricWhitelist: metricWhitelist })
