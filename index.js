5264d9a8f0499932642a0d667e6476ecde6bee5f
Ben Schwarz <ben@germanforblack.com>
2017-06-22 10:54:19 +1000

    Use src/server to export the app, and index to start it. Set bodyParser limit to 10mb

--- index.js
+++ index.js
@@ -1,65 +1,7 @@
 require('dotenv').config()
 
-const url = require('url')
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const geckoboard = require('geckoboard')(process.env.GECKOBOARD_API_KEY)
-
-const Metrics = require('./src/metrics')
-
-let metricWhitelist
-if (process.env.METRIC_WHITELIST) metricWhitelist = process.env.METRIC_WHITELIST.split(',').map(s => s.trim())
-
-const app = express()
+const server = require('./src/server')
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'hbs')
-app.set('views', __dirname + '/views')
-app.use(bodyParser.json({ strict: false }))
-app.use((err, req, res, next) => {
-  if (err) {
-    console.error('Unable to parse', req.body)
-    return res.status(500).json({ status: 'error' })
-  }
+server.listen(port, () => console.log('listening on port 3000'))
 
-  return next()
-})
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    metrics: metricWhitelist || ['all Calibre metrics'],
-    webhook_path: url.format({
-      protocol: req.protocol,
-      host: req.get('host'),
-      pathname: 'webhook'
-    })
-  })
-})
-
-app.post('/webhook', (req, res) => {
-  metrics = new Metrics({ payload: req.body, metricWhitelist: metricWhitelist })
-
-  metrics.datatables.forEach(dataset => {
-    const { id, fields, values } = dataset
-    console.log('Updating datatable', id)
-
-    geckoboard.datasets.findOrCreate({ id, fields }, (err, table) => {
-      if (err) {
-        console.error('findOrCreate', err)
-        return
-      }
-
-      table.post(values, {}, err => {
-        if (err) {
-          console.error('post', err)
-          return
-        }
-      })
-    })
-  })
-
-  res.status(200).json({ status: 'ok' })
-})
-
-app.listen(port)
---
+++ src/server.js
@@ -0,0 +1,62 @@
+const url = require('url')
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const geckoboard = require('geckoboard')(process.env.GECKOBOARD_API_KEY)
+
+const Metrics = require('./metrics')
+
+let metricWhitelist
+if (process.env.METRIC_WHITELIST) metricWhitelist = process.env.METRIC_WHITELIST.split(',').map(s => s.trim())
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', __dirname + '/views')
+app.use(bodyParser.json({ strict: false, limit: '10mb' }))
+app.use((err, req, res, next) => {
+  if (err) {
+    console.error('Unable to parse', req.body)
+    return res.status(500).json({ status: 'error' })
+  }
+
+  return next()
+})
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    metrics: metricWhitelist || ['all Calibre metrics'],
+    webhook_path: url.format({
+      protocol: req.protocol,
+      host: req.get('host'),
+      pathname: 'webhook'
+    })
+  })
+})
+
+app.post('/webhook', (req, res) => {
+  metrics = new Metrics({ payload: req.body, metricWhitelist: metricWhitelist })
+
+  metrics.datatables.forEach(dataset => {
+    const { id, fields, values } = dataset
+    console.log('Updating datatable', id)
+
+    geckoboard.datasets.findOrCreate({ id, fields }, (err, table) => {
+      if (err) {
+        console.error('findOrCreate', err)
+        return
+      }
+
+      table.post(values, {}, err => {
+        if (err) {
+          console.error('post', err)
+          return
+        }
+      })
+    })
+  })
+
+  res.status(200).json({ status: 'ok' })
+})
+
+module.exports = app
